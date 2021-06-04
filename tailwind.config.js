173d49defaa6aa8c1704b80026f81e0ac353f52e
Derek Lam <dereklam67@gmail.com>
2021-06-03 22:57:53 -0700

    feat: initial commit with sass and tailwindcss (jit) installed

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+      './pages/**/*.jsx',
+      './components/**/*.jsx'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
