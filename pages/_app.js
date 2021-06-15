e2b1ea9d8426aa072fa913b290a2e895f1a48a59
Derek Lam <dereklam67@gmail.com>
2021-06-15 12:23:31 -0700

    forgot to add polyfill

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
+
 import '../styles/main.scss';
 import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    /* 
+        Effect:
+        Dynamically import focus-visible after 
+        initial render b/c it requires document,
+        which is inaccessible when SSR until after rehydration.
+    */
+    useEffect(() => {
+        (async () => {
+            await import('focus-visible');
+        })();
+    }, []);
+
+    return <Component {...pageProps} />
 }
 
 export default MyApp
